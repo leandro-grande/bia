fd9b0e37388d4aebfcf301388d85a419bf80e5ef
Leandro Grande <leandrowg@gmail.com>
2023-11-16 09:56:46 -0300

    feat: create app

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 module.exports = {
   username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PWD || "postgres",
+  password: process.env.DB_PWD || "Mudar2020",
   database: "bia",
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: process.env.DB_PORT || 5433,
+  host: process.env.DB_HOST || "bia-database.cksmvvgxivra.us-east-1.rds.amazonaws.com",
+  port: process.env.DB_PORT || 5432,
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 };
